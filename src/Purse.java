54002618f15a90cd396b4b9fb9e0bee8fd85bdff
acrson <cstell@cub.uca.edu>
2024-08-31 17:41:50 -0500

    I added the foundations for the Denomination record as well as the Purse and Register classes.

---
+++ src/Purse.java
@@ -0,0 +1,4 @@
+public class Purse {
+    // This class doesn't need a main. Only one main is needed and it's already contained within the
+    // "Main" class. Just put purse definition and logic here.
+}
---
+++ src/Register.java
@@ -0,0 +1,7 @@
+public class Register {
+    // This class also doesn't need a main. Just put register definition and logic here.
+    public Purse makeChange(double amt)
+    {
+        // Create and return a Purse object
+    }
+}
