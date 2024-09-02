c48bad1141c30cc37f04b9a0112f0ace3598e223
acrson <cstell@cub.uca.edu>
2024-09-01 22:14:27 -0500

    Implemented Register class method "makeChange()"

--- src/Register.java
+++ src/Register.java
@@ -3,25 +3,62 @@ import java.util.Scanner;
 public class Register {
     // MAIN FUNCTION
     public static void main(String[] args) {
+        Purse purse;
         Scanner in = new Scanner(System.in);
+
         System.out.println("Enter an amount of change: ");
         double amount = in.nextDouble();
 
-        makeChange(amount);
+        purse = makeChange(amount);
+
+        System.out.println(purse.ToString());
     }
 
     // takes an amt and returns a Purse containing that amount in the fewest number of bills and coins.
     public static Purse makeChange(double amt)
     {
         Purse purse = new Purse(); // creates a new purse object to fill with money
-        double change = amt; // Saves the amt value to another variable to be used later
-        int quarters, dimes, nickels, pennies;
+        int hundreddollarbills, fiftydollarbills, twentydollarbills, tendollarbills, fivedollarbills,
+                onedollarbills, quarters, dimes, nickels, pennies; /* if there are any issues, it is likely here.
+         initialize these to zero by putting them outside the makeChange method but still within the Register class. */
 
+        // CALCULATES NUMBER OF EACH DENOMINATION NEEDED TO AMOUNT TO AMT
         // Subtract the amt comparison value by a miniscule amount to ensure no precision deviation occurs
+        for (hundreddollarbills = 0; amt >= 100.00 - 0.0001; hundreddollarbills++)
+        {
+            amt -= 100.00;
+        }
+
+        for (fiftydollarbills = 0; amt >= 50.00 - 0.0001; fiftydollarbills++)
+        {
+            amt -= 50.00;
+        }
+
+        for (twentydollarbills = 0; amt >= 20.00 - 0.0001; twentydollarbills++)
+        {
+            amt -= 20.00;
+        }
+
+        for (tendollarbills = 0; amt >= 10.00 - 0.0001; tendollarbills++)
+        {
+            amt -= 10.00;
+        }
+
+        for (fivedollarbills = 0; amt >= 5.00 - 0.0001; fivedollarbills++)
+        {
+            amt -= 5.00;
+        }
+
+        for (onedollarbills = 0; amt >= 1.00 - 0.0001; onedollarbills++)
+        {
+            amt -= 1.00;
+        }
+
         for (quarters = 0; amt >= 0.25 - 0.0001; quarters++)
         {
             amt -= 0.25;
         }
+
         for (dimes = 0; amt >= 0.10 - 0.0001; dimes++)
         {
             amt -= 0.10;
@@ -37,8 +74,17 @@ public class Register {
             amt -= 0.01;
         }
 
-        System.out.println(change + " can be counted using: \n" + quarters + " Quarters\n" + dimes + " Dimes\n" +
-                nickels + " Nickels\n" + pennies + " Pennies");
+        // MAP DENOMINATIONS TO A PURSE HASHMAP
+        purse.add(Denomination.hundreddollarbill, hundreddollarbills);
+        purse.add(Denomination.fiftydollarbill, fiftydollarbills);
+        purse.add(Denomination.twentydollarbill, twentydollarbills);
+        purse.add(Denomination.tendollarbill, tendollarbills);
+        purse.add(Denomination.fivedollarbill, fivedollarbills);
+        purse.add(Denomination.onedollarbill, onedollarbills);
+        purse.add(Denomination.quarter, quarters);
+        purse.add(Denomination.dime, dimes);
+        purse.add(Denomination.nickel, nickels);
+        purse.add(Denomination.penny, pennies);
 
         return purse;
     }
